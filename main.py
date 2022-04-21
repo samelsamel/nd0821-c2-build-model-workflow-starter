cbdf68db123530f8e4ede2d4eb7fbf9f461cea40
a.sellami <a.sellami@instadeep.com>
2022-04-21 13:09:24 +0100

    feat: add train and test random forest model

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ _steps = [
     "data_check",
     "data_split",
     "train_random_forest",
+    "test_regression_model"
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
@@ -99,20 +100,31 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],                                  
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],                   
+                    "output_artifact": "random_forest_export",            
+                },
+            )
 
-            pass
 
         if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"            
+                },
+            )
 
 if __name__ == "__main__":
     go()
