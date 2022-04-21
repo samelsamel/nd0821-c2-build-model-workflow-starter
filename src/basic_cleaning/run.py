8508200a29bd1dbf8ad8f3a6052ee93c0fe86d23
a.sellami <a.sellami@instadeep.com>
2022-04-21 13:48:59 +0100

    fix: clean data remove points not in boundary

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -40,6 +40,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # remove points outside the bounderies
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save the data
     logger.info(f'saving dataframe {args.output_artifact}')
     df.to_csv("clean_sample.csv", index=False)
